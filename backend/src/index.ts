1bc6e8d0dc91051411259d961eb4103363ae4c28
anirin <atu8823atu@gmail.com>
2025-08-11 20:47:33 +0900

    backend で todo app の list 取得 api のみ実装

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,47 @@
 import fastify from 'fastify'
+import type { FastifyInstance } from 'fastify'
+import path from 'path'
+import cors from '@fastify/cors'
+import staticPlugin from '@fastify/static'
+import { fileURLToPath } from 'url'
 
-const server = fastify()
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-server.get('/ping', async (request, reply) => {
-  return 'pong\n'
+const server : FastifyInstance = fastify()
+
+// CORS対策 全てのオリジンからのリクエストを許可
+server.register(cors, {
+  origin: '*',
 })
 
+// 静的ファイルの配信
+server.register(staticPlugin, {
+  root: path.join(__dirname, 'public'),
+})
+
+
+// Todo型
+type Todo = {
+	id: number
+	text: string
+	completed: boolean
+}
+
+let todos: Todo[] = [
+	{ id: 1, text: '野球をする', completed: false },
+	{ id: 2, text: 'ゲームをする', completed: false },
+	{ id: 3, text: '仕事をする', completed: false },
+]
+
+// GET /todos の型付きルート
+server.get<{
+	Reply: { todos: Todo[] }
+}>('/todos', async (_req, reply) => {
+	return reply.send({ todos })
+})
+
+// サーバーの起動
 server.listen({ port: 8080, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     console.error(err)
