58586b262c2c1504b592ecb0a41622b35029883c
anirin <atu8823atu@gmail.com>
2025-08-14 16:05:17 +0900

    routing の置き場所を変える

--- frontend/src/pages/home/index.ts
+++ frontend/src/pages/home/index.ts
@@ -1,6 +1,22 @@
 import { HomeModel } from "@pages/home/model/home-model";
+import homeHtml from "./ui/home.html?raw";
 
 export async function initHomePage() {
 	const homeModel = new HomeModel();
 	await homeModel.init();
+}
+
+export function renderHomePage(): void {
+	console.log("renderHomePage");
+
+	const appContainer = document.getElementById("app");
+	console.log("appContainer", appContainer);
+	if (appContainer) {
+		appContainer.innerHTML = homeHtml;
+		// ページのスクリプトを再初期化
+		initHomePage();
+		console.log("initHomePage");
+	} else {
+		console.log("appContainer not found");
+	}
 }
--- frontend/src/shared/api/api-client.ts
+++ frontend/src/shared/api/api-client.ts
@@ -2,7 +2,7 @@ export const apiClient = {
 	async get<T>(url: string): Promise<T> {
 		const response = await fetch(url);
 		if (!response.ok) {
-			throw new Error(`HTTPエラー！ステータス: ${response.status}`);
+			throw new Error(`HTTPエラー!ステータス: ${response.status}`);
 		}
 		return response.json() as Promise<T>;
 	},
