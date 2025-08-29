c2cedd2709a53156c636497deebadd964da62821
anirin <atu8823atu@gmail.com>
2025-08-29 17:40:04 +0900

    formant修正

--- frontend/src/pages/tournament/index.ts
+++ frontend/src/pages/tournament/index.ts
@@ -1,15 +1,15 @@
 import html from "./ui/tournament.html?raw";
 import "./ui/tournament.css";
-import { TournamentWebSocketAPI } from "./api/api.js";
-import { TournamentModel } from "./model/model.js";
-import { TournamentController } from "./ui/controller.js";
+// import { TournamentWebSocketAPI } from "./api/api.js";
+// import { TournamentModel } from "./model/model.js";
+// import { TournamentController } from "./ui/controller.js";
 
 export function renderTournamentPage() {
 	const app = document.getElementById("app")!;
 	app.innerHTML = html;
 
 	// 依存関係の構築: API → Model → Controller
-	const api = new TournamentWebSocketAPI();
-	const model = new TournamentModel(api);
-	const controller = new TournamentController(model);
+	// const api = new TournamentWebSocketAPI();
+	// const model = new TournamentModel(api);
+	// const controller = new TournamentController(model);
 }
