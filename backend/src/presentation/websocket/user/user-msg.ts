c4d9fc1a3d9fe420da2538c6d52a56f9a3463b90
anirin <atu8823atu@gmail.com>
2025-09-03 21:09:44 +0900

    user outgoing を削除

--- backend/src/presentation/websocket/user/user-msg.ts
+++ backend/src/presentation/websocket/user/user-msg.ts
@@ -1,13 +1,7 @@
 import type { RoomId } from "@domain/model/value-object/room/Room.js";
-import type { WSUserData } from "@domain/model/value-object/user/User.js";
 
 export type UserIncomingMsg = {
 	status: "User";
 	action: "ADD" | "DELETE";
 	room: RoomId;
-};
-
-export type UserOutgoingMsg = {
-	status: "User";
-	data: WSUserData;
 };
--- backend/src/presentation/websocket/ws-msg.ts
+++ backend/src/presentation/websocket/ws-msg.ts
@@ -1,5 +1,5 @@
 import type { RoomIncomingMsg, RoomOutgoingMsg } from "./room/room-msg.js";
-import type { UserIncomingMsg, UserOutgoingMsg } from "./user/user-msg.js";
+import type { UserIncomingMsg } from "./user/user-msg.js";
 import type { MatchIncomingMsg, MatchOutgoingMsg } from "./match/match-msg.js";
 import type { TournamentOutgoingMsg } from "./tournament/tournament-msg.js";
 
@@ -15,7 +15,6 @@ export type WSIncomingMsg =
 
 export type WSOutgoingMsg =
 | RoomOutgoingMsg
-| UserOutgoingMsg
 | MatchOutgoingMsg
 | TournamentOutgoingMsg
 | WSOutgoingMsgError
